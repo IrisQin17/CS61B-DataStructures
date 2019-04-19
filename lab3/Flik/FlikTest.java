bef6ee5740feb677338d70e2be995723759c9808
Iris Qin <irisqin17@gmail.com>
2019-04-19 17:08:33 +0800

    Done tests for Arthmetic, IntList, Flik!

---
+++ lab3/Flik/FlikTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class FlikTest {
+    @Test
+    public void test(){
+        assertTrue(Flik.isSameNumber(127, 127));
+
+        assertTrue(Flik.isSameNumber(129, 129));
+        assertTrue(Flik.isSameNumber(128, 128));
+    }
+
+}
