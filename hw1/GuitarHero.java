27a01f2a8bb3e0bad08f0411760e7ea9a0bee840
Iris Qin <irisqin17@gmail.com>
2019-05-13 20:48:39 +0800

    Done ed.datastructur.sythesizer package!

--- hw1/GuitarHero.java
+++ hw1/GuitarHero.java
@@ -1,8 +1,7 @@
 import es.datastructur.synthesizer.GuitarString;
 public class GuitarHero {
-    private static final String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
-
     public static void main(String[] args) {
+        String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
         GuitarString[] strings = new GuitarString[37];
         for (int i = 0; i < 37; i++) {
             double frequncy = 440 * Math.pow(2, (i - 24) / 12);
